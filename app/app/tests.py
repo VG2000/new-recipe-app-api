8178e715ab09d78198473797b19c445585755535
Vince <47152273+VG2000@users.noreply.github.com>
2023-10-28 17:13:48 +0100

    Fixed linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Sample tests
 """
 from django.test import SimpleTestCase
@@ -8,15 +8,15 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc moduler"""
-    
+
     def test_add_number(self):
         """Test adding numbers together"""
         res = calc.add(5,6)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
         res = calc.subtract(15, 10)
-        
+
         self.assertEqual(res, 5)
